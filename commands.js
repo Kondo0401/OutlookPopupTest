6705a33bd79b1db6d333492a020176af545ed491
Kondo0401 <masahiro_kondoh@jbcc.co.jp>
2026-08-20 17:53:09 +0900

    Update commands.js

--- commands.js
+++ commands.js
@@ -5,10 +5,16 @@ Office.onReady(() => {
 function openHQSite(event) {
     console.log("Button Click");
 
-    window.open(
+    const popup = window.open(
         "https://www.yahoo.co.jp",
         "_blank"
     );
+
+    if (popup === null) {
+        console.warn("Popup blocked");
+    } else {
+        console.log("Popup opened");
+    }
 
     event.completed();
 }
